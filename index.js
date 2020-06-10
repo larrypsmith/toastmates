41a03e5889e4e37871521b148b47e2fec81856e9
larrysmith <larrypsmith415@gmail.com>
2020-06-09 18:40:22 -0700

    Run server on port 5000

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+app.listen(5000, () => console.log('Server is running on port 5000'));
+
