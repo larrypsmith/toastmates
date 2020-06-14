0471be59e3ea2e9837a9d81bcee02b0a141d24c8
larrysmith <larrypsmith415@gmail.com>
2020-06-13 19:13:37 -0700

    Comment out old Posts logic to test new User schema:

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressGraphQL = require('express-graphql');
-const User = require('./models/User')
-const Post = require('./models/Post');
-const schema = require('./schema/schema');
+const User = require('./models/User');
+const schema = require('./schema/graphql/schema');
 
 app.use('/graphql', expressGraphQL({
   schema,
@@ -14,19 +13,20 @@ app.use('/graphql', expressGraphQL({
 
 app.use(bodyParser.json());
 
-// app.get('/users', (req, res) => {
-//   User.find({})
-//     .populate('posts')
-//     .then(user => res.json(user))
-//     .catch(err => res.json(err));
-// })
+app.post('/users', (req, res) => {
+  const user = new User({
+    name: {
+      first: 'Bird',
+      last: 'Man'
+    },
+    email: 'whatsupdoc@hello.com',
+    passwordDigest: 'passwordDigest'
+  });
 
-// app.get('/posts', (req, res) => {
-//   Post.find({})
-//     .populate('author')
-//     .then(posts => res.json(posts))
-//     .catch(err => res.json(err));
-// })
+  user.save()
+    .then(user => res.json(user))
+    .catch(err => res.json(err));
+});
 
 const db = require('./config/keys').mongoURI;
 mongoose.connect(db, { useNewUrlParser: true })
--- schema/graphql/rootQueryType.js
+++ schema/graphql/rootQueryType.js
@@ -2,9 +2,9 @@ const graphql = require('graphql');
 const { GraphQLObjectType, GraphQLList, GraphQLNonNull, GraphQLID } = graphql;
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
-const Post = mongoose.model('Post');
+// const Post = mongoose.model('Post');
 const UserType = require('./userType');
-const PostType = require('./postType');
+// const PostType = require('./postType');
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
@@ -21,20 +21,20 @@ const RootQuery = new GraphQLObjectType({
       resolve(parentValue, args) {
         return User.findById(args.id)
       }
-    },
-    posts: {
-      type: new GraphQLList(PostType),
-      resolve() {
-        return Post.find({});
-      }
-    },
-    post: {
-      type: PostType,
-      args: { id: { type: new GraphQLNonNull(GraphQLID) } },
-      resolve(parentValue, args) {
-        return Post.findById(args.id)
-      }
     }
+    // posts: {
+    //   type: new GraphQLList(PostType),
+    //   resolve() {
+    //     return Post.find({});
+    //   }
+    // },
+    // post: {
+    //   type: PostType,
+    //   args: { id: { type: new GraphQLNonNull(GraphQLID) } },
+    //   resolve(parentValue, args) {
+    //     return Post.findById(args.id)
+    //   }
+    // }
   }
 });
 
