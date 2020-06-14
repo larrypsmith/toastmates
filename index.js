84bfb0d8cd7e8ddead7ca10bbca8bb8a884fdd66
larrysmith <larrypsmith415@gmail.com>
2020-06-13 17:41:10 -0700

    Setup frontent

--- index.js
+++ index.js
@@ -7,31 +7,26 @@ const User = require('./models/User')
 const Post = require('./models/Post');
 const schema = require('./schema/schema');
 
-app.use(
-  '/graphql',
-  expressGraphQL({
-    schema,
-    graphiql: true
-  })
-);
+app.use('/graphql', expressGraphQL({
+  schema,
+  graphiql: true
+}));
 
 app.use(bodyParser.json());
 
-const router = express.Router();
+// app.get('/users', (req, res) => {
+//   User.find({})
+//     .populate('posts')
+//     .then(user => res.json(user))
+//     .catch(err => res.json(err));
+// })
 
-app.get('/users', (req, res) => {
-  User.find({})
-    .populate('posts')
-    .then(user => res.json(user))
-    .catch(err => res.json(err));
-})
-
-app.get('/posts', (req, res) => {
-  Post.find({})
-    .populate('author')
-    .then(posts => res.json(posts))
-    .catch(err => res.json(err));
-})
+// app.get('/posts', (req, res) => {
+//   Post.find({})
+//     .populate('author')
+//     .then(posts => res.json(posts))
+//     .catch(err => res.json(err));
+// })
 
 const db = require('./config/keys').mongoURI;
 mongoose.connect(db, { useNewUrlParser: true })
