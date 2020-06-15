092c2d12946d6f2e787222a27ad8f61fa1270ef3
larrysmith <larrypsmith415@gmail.com>
2020-06-14 20:47:47 -0700

    Remove Post from graphql root query

--- schema/graphql/rootQueryType.js
+++ schema/graphql/rootQueryType.js
@@ -2,9 +2,7 @@ const graphql = require('graphql');
 const { GraphQLObjectType, GraphQLList, GraphQLNonNull, GraphQLID } = graphql;
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
-// const Post = mongoose.model('Post');
 const UserType = require('./userType');
-// const PostType = require('./postType');
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
@@ -22,19 +20,6 @@ const RootQuery = new GraphQLObjectType({
         return User.findById(args.id)
       }
     }
-    // posts: {
-    //   type: new GraphQLList(PostType),
-    //   resolve() {
-    //     return Post.find({});
-    //   }
-    // },
-    // post: {
-    //   type: PostType,
-    //   args: { id: { type: new GraphQLNonNull(GraphQLID) } },
-    //   resolve(parentValue, args) {
-    //     return Post.findById(args.id)
-    //   }
-    // }
   }
 });
 
