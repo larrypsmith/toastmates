37930ae6dcad40fd5569f49a0297551d9f5e6559
Larry Smith <larrypsmith415@gmail.com>
2020-08-02 16:53:10 -0700

    Add quantity controls for adding item to cart

--- client/src/components/merchant/AddItemToCartModal.jsx
+++ client/src/components/merchant/AddItemToCartModal.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components/macro';
 import ExitModalButton from '../common/ExitModalButton';
+import Typography from '../common/Typography';
 
-const AddItemToCartModal = ({ item, ...props }) => (
-  <StyledAddItemToCartModal {...props}>
-    <StyledForm>
-      <FlexParent>
-        <ModalItemImage src={item.imgUrl} />
-        <ModalItemContent item={item} />
-      </FlexParent>
-    </StyledForm>
-  </StyledAddItemToCartModal>
-);
+const AddItemToCartModal = ({ item, ...props }) => {
+  const [quantity, setQuantity] = useState(1);
+  
+  return (
+    <StyledAddItemToCartModal {...props}>
+      <StyledForm>
+        <FlexParent>
+          <ModalItemImage src={item.imgUrl} />
+          <ModalItemContent
+            item={item}
+            quantity={quantity}
+            setQuantity={setQuantity}
+          />
+        </FlexParent>
+      </StyledForm>
+    </StyledAddItemToCartModal>
+  );
+};
 
 export default AddItemToCartModal;
 
@@ -121,10 +130,20 @@ const StyledExitModalButton = styled(ExitModalButton)`
   }
 `;
 
-const ModalItemContent = ({ item, ...props }) => (
+const ModalItemContent = ({
+  item,
+  quantity,
+  setQuantity,
+  ...props
+}) => (
   <StyledModalItemContent {...props}>
-    <ItemDetails />
-    <CartControls />
+    <ItemDetails
+      itemName={item.name}
+      description={item.description}
+      quantity={quantity}
+      setQuantity={setQuantity}
+    />
+    {/* <CartControls /> */}
   </StyledModalItemContent>
 );
 
@@ -134,10 +153,19 @@ const StyledModalItemContent = styled.div`
   width: 100%;
 `;
 
-const ItemDetails = () => (
+const ItemDetails = ({
+  itemName,
+  description,
+  quantity,
+  setQuantity
+}) => (
   <StyledItemDetails>
     <TopPaddingAndExitButton />
-    <NameAndDescription />
+    <NameAndDescription itemName={itemName} description={description} />
+    <QuantitySelector
+      quantity={quantity}
+      setQuantity={setQuantity}
+    />
   </StyledItemDetails>  
 );
 
@@ -154,7 +182,7 @@ const StyledItemDetails = styled.div`
 
 const TopPaddingAndExitButton = () => (
   <StyledTopPaddingAndExitButton>
-    
+    <ExitModalButton />
   </StyledTopPaddingAndExitButton>
 );
 
@@ -176,4 +204,125 @@ const StyledTopPaddingAndExitButton = styled.div`
     padding-left: 36px;
     padding-right: 36px;
   }
+`;
+
+const NameAndDescription = ({ itemName, description }) => (
+  <StyledNameAndDescription>
+    <NameTypography
+      as='h2'
+      size='24px'
+      weight='600'
+    > 
+      {itemName}
+    </NameTypography>
+    <Typography
+      as='p'
+      size='16'
+      color='secondary'
+    >
+      {description}
+    </Typography>
+
+  </StyledNameAndDescription>
+);
+
+const StyledNameAndDescription = styled.div`
+  padding-left: 16px;
+  padding-right: 16px;
+  margin-top: 37px;
+  margin-bottom: 30px;
+  position: relative;
+
+  @media screen and (min-width: 768px) {
+    padding-left: 24px;
+    padding-right: 24px;
+  }
+  @media screen and (min-width: 1060px) {
+    padding-top: 0px;
+    margin-top: 0px;
+  }
+`;
+
+const NameTypography = styled(Typography)`
+  margin-bottom: 8px;
+`;
+
+const QuantitySelector = ({ quantity, setQuantity }) => {
+  const decrementQuantity = (e) => {
+    e.stopPropagation();
+    setQuantity(quantity - 1);
+  };
+
+  const incrementQuantity = (e) => {
+    e.stopPropagation();
+    setQuantity(quantity + 1);
+  };
+  
+  return (
+    <StyledQuantitySelector>
+      <StyledButton
+        disabled={quantity <= 1}
+        onClick={decrementQuantity}
+      >
+        -
+      </StyledButton>
+      <StyledTypography size='16px' weight='500'>
+        {quantity}
+      </StyledTypography>
+      <StyledButton
+        disabled={quantity >=  99}
+        onClick={incrementQuantity}
+      >
+        +
+      </StyledButton>
+    </StyledQuantitySelector>
+  )
+};
+
+const StyledQuantitySelector = styled.div`
+  font-size: 16px;
+  font-weight: 500;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(255, 255, 255);
+  width: 168px;
+  max-width: 168px;
+  height: 56px;
+  color: rgb(45, 49, 56);
+  margin: 36px auto;
+  flex-shrink: 0;
+  border: 1px solid rgba(217, 219, 224, 0.5);
+  border-image: initial;
+  border-radius: 100px;
+
+  @media screen and (min-width: 1060px) {
+    display: none;
+  }
+`;
+
+const StyledTypography = styled(Typography)`
+  width: 30px;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  height: 100%;
+  width: calc(100% - 15px);
+  font-size: 16px;
+  font-weight: 500;
+
+  ${({ disabled }) => (disabled && 
+    css`
+      color: rgb(180, 184, 193);
+      pointer-events: none;
+    `
+  )}
 `;
