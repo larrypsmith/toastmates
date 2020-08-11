fa2f269c0b123cc07a761fa816f00e599ff874da
Larry Smith <larrypsmith415@gmail.com>
2020-08-11 01:00:31 -0700

    Install dotenv and get port from env

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -36,4 +37,5 @@ mongoose.connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.listen(5000, () => console.log('Server is running on port 5000'));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`))
